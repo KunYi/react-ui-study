8aca347469eebc86f6802e98eb734635c71c1aac
KunYi Chen <kunyi.chen@gmail.com>
2023-01-03 16:24:39 +0800

    modify folder strcutures

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import SignIn from './src/components/Signin/SignIn';
+import SignIn from './components/SignIn';
+
 
 function App() {
   return (
     <div className="App">
+        <div> Hello world! react!</div>
         <SignIn />
     </div>
   );
